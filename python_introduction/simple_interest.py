8becc67204bb846981ef31b05c316401af6ee91f
S063s <shaunwmanda@gmail.com>
2025-09-12 11:21:46 +0300

    add a variable of interest and print its value

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,5 +2,5 @@ principal = 1000
 rate = 0.05
 time = 3
 
-simple_interest = principal * rate * time
-print("The simple interest is:", simple_interest)
+interest = principal * rate * time
+print("The simple interest is:", interest)
