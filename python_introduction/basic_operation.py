c1e694d77382df98f5b9079c1d5ee8722f5c450d
S063s <shaunwmanda@gmail.com>
2025-09-12 10:50:56 +0300

    add new file of basic_operation.py

---
+++ python_introduction/basic_operation.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+print("The sum of", number1, "and", number2, "is:", sum_result)
+
+subtraction_result = number1 - number2
+print("The difference between", number1, "and", number2, "is:", subtraction_result)
+
+multiplication_result = number1 * number2
+print("The product of", number1, "and", number2, "is:", multiplication_result)
+
