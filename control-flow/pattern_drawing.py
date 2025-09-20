207b2e4ea4eabfb4bafe4416a885ca3e029a794e
Shaun Manda <shaunwmanda@gmail.com>
2025-09-20 11:41:38 +0300

    add asterisk symbol to pattern drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -4,8 +4,8 @@ symbol = input("Enter the symbol to use in the pattern: ")
 for i in range(1, size + 1):
     j = 0
     while j in range(size - i):
-        print(" ", end="")
+        print("*", end="")
         j += 1
     for k in range(i):
-        print(symbol, end="")
+        print("*", end="")
     print()
