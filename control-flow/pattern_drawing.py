823e37da3c39aa4ca5090ecc84a5686936920862
Shaun Manda <shaunwmanda@gmail.com>
2025-09-20 11:29:19 +0300

    add pattern drawing feature to control-flow module

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,10 +1,9 @@
-length = int(input("Enter the length of the pattern: "))
+size = int(input("Enter the size of the pattern: "))
 symbol = input("Enter the symbol to use in the pattern: ")
 
-for i in range(1, length + 1):
-    for j in range(length - i):
+for i in range(1, size + 1):
+    for j in range(size - i):
         print(" ", end="")
     for k in range(i):
         print(symbol, end="")
     print()
-    
