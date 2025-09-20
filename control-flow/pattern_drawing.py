cb31e7fdaad136db626dfd4a6ad1f8a0b3ea7f17
Shaun Manda <shaunwmanda@gmail.com>
2025-09-20 11:36:44 +0300

    add while loop to draw pattern

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,8 +2,10 @@ size = int(input("Enter the size of the pattern: "))
 symbol = input("Enter the symbol to use in the pattern: ")
 
 for i in range(1, size + 1):
-    for j in range(size - i):
+    j = 0
+    while j in range(size - i):
         print(" ", end="")
+        j += 1
     for k in range(i):
         print(symbol, end="")
     print()
