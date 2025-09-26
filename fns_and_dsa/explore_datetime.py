43b08e0fb7949a8b1e053a8083a97c7d43f856c8
Shaun Manda <shaunwmanda@gmail.com>
2025-09-26 13:40:33 +0300

    add new file to calculate date difference

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,13 @@
+def display_current_datetime():
+    from datetime import datetime
+    current_date = datetime.now()
+    formatted_datetime = current_date.strftime("YYYY-MM-DD HH:MM:SS")
+    print(f"Enter a number of days: {formatted_datetime}")
+
+def calculate_future_date():
+    from datetime import datetime, timedelta
+    future_days = int(input("Enter a number of days: ").strip())
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=future_days)
+    formatted_future_date = future_date.strftime("YYYY-MM-DD")
+    print(f"Future date after {future_days} days will be: {formatted_future_date}")
