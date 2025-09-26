d2146a2b1868aebbf1745090275594fd8f2f9e3d
Shaun Manda <shaunwmanda@gmail.com>
2025-09-26 14:46:36 +0300

    .

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -9,7 +9,7 @@ def convert_to_fahrenheit(celsius):
 
 def main():
     print("Enter the temperature to convert:")
-    choice = input("Is the temperature in Fahrenheit or Celsius? (F/C): ").strip().upper()
+    choice = input("Is this temperature in Fahrenheit or Celsius? (F/C): ").strip().upper()
     if choice == 'F':
         fahrenheit = float(input("Enter the temperature to convert in Fahrenheit: "))
         celsius = convert_to_celsius(fahrenheit)
