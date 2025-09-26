ea41ee5d4c4281c2cb0078691957e80b337acc6e
Shaun Manda <shaunwmanda@gmail.com>
2025-09-26 13:45:25 +0300

    add format on the formatting function

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,7 +1,7 @@
 def display_current_datetime():
     from datetime import datetime
     current_date = datetime.now()
-    formatted_datetime = current_date.strftime("YYYY-MM-DD HH:MM:SS")
+    formatted_datetime = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Enter a number of days: {formatted_datetime}")
 
 def calculate_future_date():
