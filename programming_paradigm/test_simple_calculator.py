6074efc0356ba73751be54c06be6566a5d09ce98
Shaun Manda <shaunwmanda@gmail.com>
2025-10-05 13:48:45 +0300

    fix typing error in function names in simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,23 +5,23 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calculator = SimpleCalculator()
 
-    def test_add(self):
-        self.assertEqual(self.calculator.add(2, 3), 5)
-        self.assertEqual(self.calculator.add(-1, 1), 0)
+    def test_addition(self):
+        self.assertEqual(self.cal.add(2, 3), 5)
+        self.assertEqual(self.cal.add(-1, 1), 0)
 
-    def test_subtract(self):
-        self.assertEqual(self.calculator.subtract(5, 3), 2)
-        self.assertEqual(self.calculator.subtract(2, 2), 0)
+    def test_subtraction(self):
+        self.assertEqual(self.cal.subtract(5, 3), 2)
+        self.assertEqual(self.cal.subtract(2, 2), 0)
 
-    def test_multiply(self):
-        self.assertEqual(self.calculator.multiply(2, 3), 6)
-        self.assertEqual(self.calculator.multiply(-1, 1), -1)
+    def test_multiplication(self):
+        self.assertEqual(self.cal.multiply(2, 3), 6)
+        self.assertEqual(self.cal.multiply(-1, 1), -1)
 
-    def test_divide(self):
-        self.assertEqual(self.calculator.divide(6, 3), 2)
-        self.assertEqual(self.calculator.divide(5, 2), 2.5)
+    def test_division(self):
+        self.assertEqual(self.cal.divide(6, 3), 2)
+        self.assertEqual(self.cal.divide(5, 2), 2.5)
         with self.assertRaises(ZeroDivisionError):
-            self.calculator.divide(1, 0)
+            self.cal.divide(1, 0)
 
 if __name__ == '__main__':
     unittest.main()
