7d8b8fc9e9bfa38c14ddd66c7d90b8c40768be14
Shaun Manda <shaunwmanda@gmail.com>
2025-10-05 13:53:54 +0300

    add self.calc.add for testing addition operation

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,20 +6,20 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calculator = SimpleCalculator()
 
     def test_addition(self):
-        self.assertEqual(self.cal.add(2, 3), 5)
-        self.assertEqual(self.cal.add(-1, 1), 0)
+        self.assertEqual(self.cal.add)
+        self.assertEqual(self.cal.add)
 
     def test_subtraction(self):
-        self.assertEqual(self.cal.subtract(5, 3), 2)
-        self.assertEqual(self.cal.subtract(2, 2), 0)
+        self.assertEqual(self.cal.subtract)
+        self.assertEqual(self.cal.subtract)
 
     def test_multiplication(self):
-        self.assertEqual(self.cal.multiply(2, 3), 6)
-        self.assertEqual(self.cal.multiply(-1, 1), -1)
+        self.assertEqual(self.cal.multiply)
+        self.assertEqual(self.cal.multiply)
 
     def test_division(self):
-        self.assertEqual(self.cal.divide(6, 3), 2)
-        self.assertEqual(self.cal.divide(5, 2), 2.5)
+        self.assertEqual(self.cal.divide)
+        self.assertEqual(self.cal.divide)
         with self.assertRaises(ZeroDivisionError):
             self.cal.divide(1, 0)
 
